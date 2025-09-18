7dc9d9840a6c84c258e1b59a29d45d68b3986edf
omarxe31 <87352757+omarxe31@users.noreply.github.com>
2025-09-18 18:23:57 +0300

    Update PluginEditor.h

--- PluginEditor.h
+++ PluginEditor.h
@@ -1,11 +1,9 @@
 #pragma once
 
-#include <JuceHeader.h>
+#include <juce_gui_extra/juce_gui_extra.h>
 #include "PluginProcessor.h"
 
-class EltraV1AudioProcessorEditor  : public juce::AudioProcessorEditor,
-                                     public juce::Slider::Listener,
-                                     public juce::Button::Listener
+class EltraV1AudioProcessorEditor  : public juce::AudioProcessorEditor
 {
 public:
     EltraV1AudioProcessorEditor (EltraV1AudioProcessor&);
@@ -14,19 +12,7 @@ public:
     void paint (juce::Graphics&) override;
     void resized() override;
 
-    void sliderValueChanged(juce::Slider* slider) override;
-    void buttonClicked(juce::Button* button) override;
-
 private:
     EltraV1AudioProcessor& processorRef;
-
-    juce::Slider mixSlider;
-    juce::Slider decaySlider;
-    juce::Slider preDelaySlider;
-    juce::Slider toneSlider;
-
-    juce::ToggleButton softHardButton; // Soft/Hard mode toggle
-    juce::ToggleButton lightDarkButton; // Light/Dark mode
-
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (EltraV1AudioProcessorEditor)
 };
