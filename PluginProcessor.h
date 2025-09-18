ee79b4cd3fde80505f014e8cb3da8f216fbc556b
omarxe31 <87352757+omarxe31@users.noreply.github.com>
2025-09-18 18:22:57 +0300

    Update PluginProcessor.h

--- PluginProcessor.h
+++ PluginProcessor.h
@@ -1,6 +1,7 @@
 #pragma once
 
-#include <JuceHeader.h>
+#include <juce_audio_processors/juce_audio_processors.h>
+#include <juce_dsp/juce_dsp.h> // علشان تستخدم DSP زي Reverb و ProcessSpec
 
 class EltraV1AudioProcessor  : public juce::AudioProcessor
 {
@@ -10,18 +11,12 @@ public:
 
     void prepareToPlay (double sampleRate, int samplesPerBlock) override;
     void releaseResources() override;
-
-   #ifndef JucePlugin_PreferredChannelConfigurations
-    bool isBusesLayoutSupported (const BusesLayout& layouts) const override;
-   #endif
-
     void processBlock (juce::AudioBuffer<float>&, juce::MidiBuffer&) override;
 
     juce::AudioProcessorEditor* createEditor() override;
     bool hasEditor() const override;
 
     const juce::String getName() const override;
-
     bool acceptsMidi() const override;
     bool producesMidi() const override;
     bool isMidiEffect() const override;
@@ -31,21 +26,13 @@ public:
     int getCurrentProgram() override;
     void setCurrentProgram (int index) override;
     const juce::String getProgramName (int index) override;
-    void changeProgramName (int, const juce::String& newName) override;
+    void changeProgramName (int index, const juce::String& newName) override;
 
     void getStateInformation (juce::MemoryBlock& destData) override;
     void setStateInformation (const void* data, int sizeInBytes) override;
 
-    // DSP Parameters
-    juce::AudioParameterFloat* mixParam;
-    juce::AudioParameterFloat* decayParam;
-    juce::AudioParameterFloat* preDelayParam;
-    juce::AudioParameterFloat* toneParam;
-    juce::AudioParameterBool* softModeParam;
-
 private:
-    juce::dsp::Reverb reverb;
-    juce::dsp::Reverb::Parameters reverbParams;
-
+    juce::Reverb reverb; // مؤثر الصوت الأساسي
+    juce::dsp::ProcessSpec spec; // مواصفات الـ DSP
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (EltraV1AudioProcessor)
 };
