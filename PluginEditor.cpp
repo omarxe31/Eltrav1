9d399851aa5880efd06c43dd4a143a9ef3d44c5f
omarxe31 <87352757+omarxe31@users.noreply.github.com>
2025-09-19 20:16:45 +0300

    Update PluginEditor.cpp

--- PluginEditor.cpp
+++ PluginEditor.cpp
@@ -1,34 +1,51 @@
 #include "PluginEditor.h"
 
 EltraV1AudioProcessorEditor::EltraV1AudioProcessorEditor (EltraV1AudioProcessor& p)
-    : AudioProcessorEditor(&p), processor(p)
+    : AudioProcessorEditor(&p), processorRef(p)
 {
     setSize(400, 300);
 
-    gainSlider.setSliderStyle(juce::Slider::Rotary);
-    gainSlider.setTextBoxStyle(juce::Slider::TextBoxBelow, false, 60, 20);
-    addAndMakeVisible(gainSlider);
+    titleLabel.setText("Al8iser Reverb", juce::dontSendNotification);
+    titleLabel.setJustificationType(juce::Justification::centred);
+    titleLabel.setFont(juce::Font(20.0f, juce::Font::bold));
+    addAndMakeVisible(titleLabel);
 
-    gainLabel.setText("Gain", juce::dontSendNotification);
-    gainLabel.attachToComponent(&gainSlider, false);
-    gainLabel.setJustificationType(juce::Justification::centred);
-    addAndMakeVisible(gainLabel);
+    auto setupSlider = [&](juce::Slider& slider, const juce::String& name)
+    {
+        slider.setSliderStyle(juce::Slider::Rotary);
+        slider.setTextBoxStyle(juce::Slider::TextBoxBelow, false, 60, 20);
+        slider.setName(name);
+        addAndMakeVisible(slider);
+    };
 
-    gainAttachment = std::make_unique<juce::AudioProcessorValueTreeState::SliderAttachment>(
-        processor.parameters, "gain", gainSlider);
+    setupSlider(roomSizeSlider, "Room Size");
+    setupSlider(dampingSlider,  "Damping");
+    setupSlider(widthSlider,    "Width");
+    setupSlider(mixSlider,      "Mix");
+
+    roomSizeAttachment = std::make_unique<juce::AudioProcessorValueTreeState::SliderAttachment>(processorRef.parameters, "roomSize", roomSizeSlider);
+    dampingAttachment  = std::make_unique<juce::AudioProcessorValueTreeState::SliderAttachment>(processorRef.parameters, "damping",  dampingSlider);
+    widthAttachment    = std::make_unique<juce::AudioProcessorValueTreeState::SliderAttachment>(processorRef.parameters, "width",    widthSlider);
+    mixAttachment      = std::make_unique<juce::AudioProcessorValueTreeState::SliderAttachment>(processorRef.parameters, "mix",      mixSlider);
 }
-
-EltraV1AudioProcessorEditor::~EltraV1AudioProcessorEditor() {}
 
 void EltraV1AudioProcessorEditor::paint (juce::Graphics& g)
 {
-    g.fillAll(juce::Colours::darkgrey);
+    g.fillAll(juce::Colours::darkslategrey);
     g.setColour(juce::Colours::white);
-    g.setFont(20.0f);
-    g.drawFittedText("EltraV1 - Gain Control", getLocalBounds(), juce::Justification::centredTop, 1);
+    g.drawRect(getLocalBounds(), 1);
 }
 
 void EltraV1AudioProcessorEditor::resized()
 {
-    gainSlider.setBounds(getWidth()/2 - 50, getHeight()/2 - 50, 100, 100);
+    titleLabel.setBounds(0, 10, getWidth(), 30);
+    
+    auto area = getLocalBounds().reduced(40);
+    auto sliderArea = area.removeFromBottom(area.getHeight() - 50);
+    int sliderWidth = sliderArea.getWidth() / 4;
+
+    roomSizeSlider.setBounds(sliderArea.removeFromLeft(sliderWidth).reduced(10));
+    dampingSlider.setBounds(sliderArea.removeFromLeft(sliderWidth).reduced(10));
+    widthSlider.setBounds(sliderArea.removeFromLeft(sliderWidth).reduced(10));
+    mixSlider.setBounds(sliderArea.removeFromLeft(sliderWidth).reduced(10));
 }
