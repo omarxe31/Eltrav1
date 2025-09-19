33a623b835a55992b720f032bbb148816c7af5d2
omarxe31 <87352757+omarxe31@users.noreply.github.com>
2025-09-19 20:30:53 +0300

    Update PluginEditor.h

--- PluginEditor.h
+++ PluginEditor.h
@@ -1,23 +1,17 @@
 #pragma once
-#include <JuceHeader.h>
+#include <juce_gui_basics/juce_gui_basics.h>
 #include "PluginProcessor.h"
 
-class EltraV1AudioProcessorEditor  : public juce::AudioProcessorEditor
+class EltraV1AudioProcessorEditor : public juce::AudioProcessorEditor
 {
 public:
-    EltraV1AudioProcessorEditor (EltraV1AudioProcessor&);
+    EltraV1AudioProcessorEditor(EltraV1AudioProcessor&);
     ~EltraV1AudioProcessorEditor() override = default;
 
-    void paint (juce::Graphics&) override;
+    void paint(juce::Graphics&) override;
     void resized() override;
 
 private:
-    EltraV1AudioProcessor& processorRef;
-
-    juce::Slider roomSizeSlider, dampingSlider, widthSlider, mixSlider;
-    juce::Label titleLabel;
-
-    std::unique_ptr<juce::AudioProcessorValueTreeState::SliderAttachment> roomSizeAttachment, dampingAttachment, widthAttachment, mixAttachment;
-
-    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (EltraV1AudioProcessorEditor)
+    EltraV1AudioProcessor& processor;
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(EltraV1AudioProcessorEditor)
 };
