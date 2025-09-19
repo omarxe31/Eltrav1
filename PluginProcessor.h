3e86bfd5b49d811dfeaf9da175e1af99f3c34c33
omarxe31 <87352757+omarxe31@users.noreply.github.com>
2025-09-19 19:58:59 +0300

    Update PluginProcessor.h

--- PluginProcessor.h
+++ PluginProcessor.h
@@ -1,43 +1,44 @@
 #pragma once
-#include <JuceHeader.h>
 
+#include <juce_audio_processors/juce_audio_processors.h>
+#include <juce_dsp/juce_dsp.h>
+
+//==============================================================================
 class EltraV1AudioProcessor  : public juce::AudioProcessor
 {
 public:
     EltraV1AudioProcessor();
     ~EltraV1AudioProcessor() override;
 
+    //==============================================================================
     void prepareToPlay (double sampleRate, int samplesPerBlock) override;
     void releaseResources() override;
     void processBlock (juce::AudioBuffer<float>&, juce::MidiBuffer&) override;
 
+    //==============================================================================
     juce::AudioProcessorEditor* createEditor() override;
     bool hasEditor() const override;
 
+    //==============================================================================
     const juce::String getName() const override;
     bool acceptsMidi() const override;
     bool producesMidi() const override;
+    bool isMidiEffect() const override;
     double getTailLengthSeconds() const override;
 
+    //==============================================================================
     int getNumPrograms() override;
     int getCurrentProgram() override;
     void setCurrentProgram (int index) override;
     const juce::String getProgramName (int index) override;
-    void changeProgramName (int, const juce::String& newName) override;
+    void changeProgramName (int index, const juce::String& newName) override;
 
+    //==============================================================================
     void getStateInformation (juce::MemoryBlock& destData) override;
     void setStateInformation (const void* data, int sizeInBytes) override;
 
-    // Expose APVTS so editor can attach sliders
-    juce::AudioProcessorValueTreeState apvts;
-
-    static juce::AudioProcessorValueTreeState::ParameterLayout createParameterLayout();
-
 private:
     juce::dsp::Reverb reverb;
-    juce::dsp::Reverb::Parameters reverbParams;
-    juce::AudioBuffer<float> wetBuffer;
     juce::dsp::ProcessSpec spec;
-
+    
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (EltraV1AudioProcessor)
-};
